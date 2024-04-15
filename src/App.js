7fc0260f59e652235fc3937521c8165681015005
Chris Hughes <chughes@illio.com>
2024-04-15 15:33:43 +0100

    Add swipe left and right to change video

--- src/App.js
+++ src/App.js
@@ -30,29 +30,33 @@ function App() {
       haiku: "Storm-tossed waves crash down,<br>Overwhelmed by nature's might,<br>Seeking calm within.",
       videoUrl: "https://upload.wikimedia.org/wikipedia/commons/transcoded/8/85/Rough_Sea_at_Dover_1896_Birt_Acres_Robert_W_Paul.webm/Rough_Sea_at_Dover_1896_Birt_Acres_Robert_W_Paul.webm.720p.vp9.webm",
       hashtags: ["storm", "overwhelmed"]
-    },
-    {
-      title: "Plain Sailing 2",
-      haiku: "Test Removed when plain sailing one removed",
-      videoUrl: "https://upload.wikimedia.org/wikipedia/commons/transcoded/8/85/Rough_Sea_at_Dover_1896_Birt_Acres_Robert_W_Paul.webm/Rough_Sea_at_Dover_1896_Birt_Acres_Robert_W_Paul.webm.720p.vp9.webm",
-      hashtags: ["storm", "test data"]
     }
   ];
 
   const [videoData, setVideoData] = useState(initialVideoData);
   const [videoIdx, setVideoIdx] = useState(0);
+  const [favorites, setFavorites] = useState([]);
 
-
+// removes video from list if it has any of the same hashtags as the current video
   const dontLikeVideo = () => {
-    if (videoData.length > 1) {
-      let currentVideo = videoData[videoIdx];
-      let updatedVd = videoData.filter(v => !currentVideo.hashtags.some(r => v.hashtags.includes(r)));
+    console.log("dontLikeVideo idx: ", videoIdx, " length: ", videoData.length);
+    let currentVideo = videoData[videoIdx];
+    let updatedVd = videoData.filter(v => !currentVideo.hashtags.some(r => v.hashtags.includes(r)));
+    if (updatedVd.length > 1) {
       setVideoData(updatedVd);
       setVideoIdx((videoIdx + 1) % updatedVd.length); // Change to the URL of the new video, loop roound if necessary
     }
   };
 
+  // adds hashtags to favorites and moves to the next video
   const likeVideo = () => {
+    console.log("favorites: ", favorites, " videoData[videoIdx].hashtags: ", ...videoData[videoIdx].hashtags)
+    setFavorites([...favorites, ...videoData[videoIdx].hashtags]);
+    nextVideo();
+  };
+
+  const nextVideo = () => {
+    console.log("nextVideo idx: ", videoIdx, " length: ", videoData.length);
     setVideoIdx((videoIdx + 1) % videoData.length);
   };
 
@@ -62,14 +66,16 @@ function App() {
         <div class="cs-container">
           <div class="cs-content">
             <span class="cs-topper">Clip Clop</span>
-            <h2 class="cs-title"> {videoData[videoIdx].title}</h2>
+            <h2 class="cs-title"> {videoData[videoIdx].title}
+              {favorites.some(r => videoData[videoIdx].hashtags.includes(r)) && <img src='heart-icon-3335.png' height="40" width="auto" />}
+            </h2>
             <span class="cs-topper">
               {videoData[videoIdx].hashtags.map((hashtag, idx) => (
                 <span key={idx}>#{hashtag}&nbsp;</span>
               ))}
             </span>
             <picture class="cs-picture">
-              <Video video_source_url={videoData[videoIdx].videoUrl} />
+              <Video video_source_url={videoData[videoIdx].videoUrl} dontLikeVideo={dontLikeVideo} nextVideo={nextVideo} likeVideo={likeVideo} />
             </picture>
           </div>
 
@@ -84,7 +90,7 @@ function App() {
               <img class="cs-icon" loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/Icons/red-layer.svg" alt="icon" width="32" height="32" />
               <div class="cs-item-wrapper">
                 <span><button class="cs-button-solid" onClick={dontLikeVideo}><p class="cs-item-text">Not for Me</p></button>&nbsp;&nbsp;
-                  <button class="cs-button-solid" onClick={likeVideo}><p class="cs-item-text">Next</p></button></span>
+                  <button class="cs-button-solid" onClick={nextVideo}><p class="cs-item-text">Next</p></button></span>
               </div>
             </li>
 
--- src/video.js
+++ src/video.js
@@ -1,10 +1,24 @@
 import React, { useRef, useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
+// Swipable Video player
+function Video({ video_source_url, dontLikeVideo, nextVideo, likeVideo}) {
+    let start = Date.parse('01 Jan 1970 00:00:00 GMT');
 
 
-function Video({ video_source_url }) {
+    const handlers = useSwipeable({
+        onSwipedRight: (eventData) => dontLikeVideo(),
+        onSwipedLeft: (eventData) => nextVideo(),
+        onTap: (eventData) => {
+            if(start + 500 > Date.now())
+                likeVideo();
+            else start = Date.now();
+        },
+        trackMouse: true
+      });
+
     return (
-        <div style={{overflow: 'wrap', width: '100%', height: '360px',  maxWidth: '100%'}}>
-            <video className="video__player" autoPlay muted height="auto" width="100%" src={video_source_url} />
+        <div {...handlers} style={{ overflow: 'wrap', width: '100%', height: '360px', maxWidth: '100%' }}>
+            <video {...handlers} className="video__player" autoPlay muted height="auto" width="100%" src={video_source_url} />
         </div>
     );
 }
